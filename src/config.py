7a15028c9ca40e2f12092308fd9c28eb62abbe36
mutuiris <muneneaddy@gmail.com>
2026-07-04 10:59:47 +0300

    Add configuration file for environment variables and API keys

---
+++ src/config.py
@@ -0,0 +1,17 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+TWILIO_ACCOUNT_SID = os.environ["TWILIO_ACCOUNT_SID"]
+TWILIO_AUTH_TOKEN = os.environ["TWILIO_AUTH_TOKEN"]
+TWILIO_WHATSAPP_NUMBER = os.environ.get("TWILIO_WHATSAPP_NUMBER", "whatsapp:+14155238886")
+
+DEEPSEEK_API_KEY = os.environ.get("DEEPSEEK_API_KEY", "")
+DEEPSEEK_BASE_URL = "https://api.deepseek.com"
+HF_API_TOKEN = os.environ.get("HF_API_TOKEN", "")
+GOOGLE_FACTCHECK_API_KEY = os.environ.get("GOOGLE_FACTCHECK_API_KEY", "")
+FIRECRAWL_API_KEY = os.environ.get("FIRECRAWL_API_KEY", "")
+
+AT_USERNAME = os.environ.get("AT_USERNAME", "")
+AT_API_KEY = os.environ.get("AT_API_KEY", "")

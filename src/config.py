f822513e1157b2809f43235b6555f9b217841003
mutuiris <muneneaddy@gmail.com>
2026-07-04 12:55:25 +0300

    Remove Google Fact Check API key from configuration

--- src/config.py
+++ src/config.py
@@ -11,7 +11,6 @@ DEEPSEEK_API_KEY = os.environ.get("DEEPSEEK_API_KEY", "")
 DEEPSEEK_BASE_URL = "https://api.deepseek.com"
 HF_API_TOKEN = os.environ.get("HF_API_TOKEN", "")
 GROQ_API_KEY = os.environ.get("GROQ_API_KEY", "")
-GOOGLE_FACTCHECK_API_KEY = os.environ.get("GOOGLE_FACTCHECK_API_KEY", "")
 FIRECRAWL_API_KEY = os.environ.get("FIRECRAWL_API_KEY", "")
 
 AT_USERNAME = os.environ.get("AT_USERNAME", "")
